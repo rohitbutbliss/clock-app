4cbfb0935123550627d7c5e6dbbab9cc9b104a22
rohitbutbliss <kumarrohit6324@gmail.com>
2023-03-07 14:40:28 +0530

    fixed clock sizes across all screens

--- index.js
+++ index.js
@@ -3,9 +3,16 @@ let hours = date.getHours() % 12;
 let minutes = date.getMinutes();
 let seconds = date.getSeconds();
 
+const clock = document.getElementById("clock");
 const hourHand = document.getElementById("hour");
 const minuteHand = document.getElementById("minute");
 const secondHand = document.getElementById("second");
+
+const screenHeight = screen.height;
+const screenWidth = screen.width;
+
+if (screenHeight > screenWidth) clock.className = "mobile-screen";
+else clock.className = "desktop-screen";
 
 let totalSeconds = (hours * 3600 + minutes * 60 + seconds) % 43200;
 
@@ -29,5 +36,8 @@ function runClock() {
   secondHand.style.transform = `translate(-50%, -100%) rotate(${secondDeg}deg)`;
   if (totalSeconds % 60 == 0) secondDeg = 0;
   if (totalSeconds % 3600 == 0) minuteDeg = 0;
-  if (totalSeconds % 43200 == 0) hourDeg = 0;
+  if (totalSeconds % 43200 == 0) {
+    totalSeconds = 0;
+    hourDeg = 0;
+  }
 }
