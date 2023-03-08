bd141277a5809b9514d01373b9012f43e649b9f8
rohitbutbliss <kumarrohit6324@gmail.com>
2023-03-08 21:28:57 +0530

    improvised for small screens

--- index.js
+++ index.js
@@ -28,12 +28,15 @@ setInterval(runClock, 1000);
 
 function runClock() {
   totalSeconds++;
+
   hourDeg += 1 / 120;
   minuteDeg += 1 / 10;
   secondDeg += 6;
+
   hourHand.style.transform = `translate(-50%, -100%) rotate(${hourDeg}deg)`;
   minuteHand.style.transform = `translate(-50%, -100%) rotate(${minuteDeg}deg)`;
   secondHand.style.transform = `translate(-50%, -100%) rotate(${secondDeg}deg)`;
+
   if (totalSeconds % 60 == 0) secondDeg = 0;
   if (totalSeconds % 3600 == 0) minuteDeg = 0;
   if (totalSeconds % 43200 == 0) {
